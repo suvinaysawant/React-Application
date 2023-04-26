88a3660945ffe60f4983653bed54e7a87b87a245
suvinaysawant <suvinaysawant.ss@gmail.com>
2023-04-26 23:54:33 +0530

    To Do App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState,useEffect } from 'react';
 function App() {
+
+  const [task, setTask] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function createTodo(){
+    setTodos(oldTodos =>{
+      return [...oldTodos,task]
+    })
+  }
+  
   return (
-    <div>Hi
+    <div>
+      <h1>To Do App</h1>
+      <input type="text" value={task} onChange={event =>{
+        setTask(event.target.value)
+      }}
+      />
+      <button onClick={createTodo}>Create Todo</button>
+   
+      <ul>
+        {todos.map(todo => {
+        return <li>{todo}</li>
+         } )}
+      </ul>
     </div>
   );
 }
