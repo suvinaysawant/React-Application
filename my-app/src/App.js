14cb6732b83adb12fa7ae0e2c0199838b569797c
suvinaysawant <suvinaysawant.ss@gmail.com>
2023-04-27 23:57:41 +0530

    To Do App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState,useEffect } from 'react';
+
+let globalID = 0;
+
 function App() {
 
   const [task, setTask] = useState("")
   const [todos, setTodos] = useState([])
 
   function createTodo(){
-    setTodos(oldTodos =>{
-      return [...oldTodos,task]
+    setTodos(oldTodos => {
+      setTask('')
+      return [...oldTodos, {todo: task, id: globalID++ }]
     })
+  }
+
+  function tryToCheckForEnterKey(e) {
+
+    if(e.keyCode === 13){
+      createTodo()
+    }
+  }
+
+  function deleteItem(itemID) {
+      setTodos(oldTodos => oldTodos.filter(item => item.id !== itemID))
+
   }
   
   return (
-    <div>
+    <div className="container">
       <h1>To Do App</h1>
-      <input type="text" value={task} onChange={event =>{
-        setTask(event.target.value)
+      <input
+      className='textbox'
+          onKeyDown={tryToCheckForEnterKey}
+          type="text" 
+         value={task}
+         onChange={event =>{
+         setTask(event.target.value)
       }}
       />
-      <button onClick={createTodo}>Create Todo</button>
-   
+      <button onClick={createTodo} className='button'>Create Todo</button>
+      <div className='box'>
       <ul>
-        {todos.map(todo => {
-        return <li>{todo}</li>
-         } )}
+        {todos.map((item, index) => {
+        return <div key={item.id}>
+        <li>{item.todo}</li>
+        <button onClick= {() => deleteItem(item.id)}> Delete</button>
+        </div>
+         }) }
       </ul>
+      </div>
     </div>
   );
 }
